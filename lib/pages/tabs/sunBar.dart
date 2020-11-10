01d3b38a9b290001aad89e8cf45fbdbe62658e0f
Jessice <jessicesun@gmail.com>
2020-11-10 17:49:52 +0800

    Flutter TitleBar 切换

--- lib/pages/tabs/sunBar.dart
+++ lib/pages/tabs/sunBar.dart
@@ -17,34 +17,73 @@ class sunBarSon extends State{
   Widget build(BuildContext context) {
     // TODO: implement build
     //throw UnimplementedError();
-    return Scaffold(
-      appBar: AppBar(
-        //1、TitleBar 导航背景颜色
-        backgroundColor: Colors.blueAccent,
-        //2、TitleBar 前指定图标，图标没法点击，加上默认返回按钮消失
-        //leading: Icon(Icons.list,color: Colors.white,),
-        //3、TitleBar 前指定图标，图标可点击，加上默认返回按钮消失
-        leading: IconButton(
-          icon: Icon(Icons.list,color: Colors.white,),
-          onPressed: (){
-            print("你点击了功能菜单");
-          },
-        ),
-        //4、TitleBar 后边添加图标
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search,color: Colors.white,),
-            onPressed: (){
-              print("你点击了搜索按钮");
-            },
+    return DefaultTabController(
+      //顶部Tab切换数量
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          //1、TitleBar 导航背景颜色
+          backgroundColor: Colors.blueAccent,
+          //2、TitleBar 前指定图标，图标没法点击，加上默认返回按钮消失
+          //leading: Icon(Icons.list,color: Colors.white,),
+          //3、TitleBar 前指定图标，图标可点击，加上默认返回按钮消失
+          // leading: IconButton(
+          //   icon: Icon(Icons.list,color: Colors.white,),
+          //   onPressed: (){
+          //     print("你点击了功能菜单");
+          //   },
+          // ),
+          //4、TitleBar 后边添加图标
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search,color: Colors.white,),
+              onPressed: (){
+                print("你点击了搜索按钮");
+              },
+            ),
+          ],
+          //5、文本居中
+          centerTitle: true,
+          title: Text("aaa"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(text:"热门"),
+              Tab(text:"推荐"),
+            ],
           ),
-        ],
-        //5、文本居中
-        centerTitle: true,
-        title: Text("aaa"),
+        ),
+        body: TabBarView(
 
+          children: <Widget>[
+            ListView(
+              children: [
+                ListTile(
+                  title: Text("列表1"),
+                ),
+                ListTile(
+                  title: Text("列表1"),
+                ),
+                ListTile(
+                  title: Text("列表1"),
+                ),
+              ],
+            ),
+            ListView(
+              children: [
+                ListTile(
+                  title: Text("列表2"),
+                ),
+                ListTile(
+                  title: Text("列表2"),
+                ),
+                ListTile(
+                  title: Text("列表2"),
+                )
+              ],
+            )
+          ],
+        ),
       ),
-      body: Text("aaa"),
     );
   }
 
--- lib/pages/tabs/sunCategory.dart
+++ lib/pages/tabs/sunCategory.dart
@@ -15,8 +15,76 @@ class sunCategorySon extends State{
   Widget build(BuildContext context) {
     // TODO: implement build
     //throw UnimplementedError();
-    return Container(
-      child: Text("Category页面"),
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TabBar(
+                    tabs: <Widget>[
+                      Tab(text: "热销",),
+                      Tab(text: "推荐",),
+                      Tab(text: "热门",),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            // bottom: TabBar(
+            //   tabs: <Widget>[
+            //     Tab(text: "热销",),
+            //     Tab(text: "推荐",),
+            //     Tab(text: "热门",),
+            //   ],
+            // ),
+          ),
+          body: TabBarView(
+
+            children: <Widget>[
+              ListView(
+                children: [
+                  ListTile(
+                    title: Text("列表1"),
+                  ),
+                  ListTile(
+                    title: Text("列表1"),
+                  ),
+                  ListTile(
+                    title: Text("列表1"),
+                  ),
+                ],
+              ),
+              ListView(
+                children: [
+                  ListTile(
+                    title: Text("列表2"),
+                  ),
+                  ListTile(
+                    title: Text("列表2"),
+                  ),
+                  ListTile(
+                    title: Text("列表2"),
+                  )
+                ],
+              ),
+              ListView(
+                children: [
+                  ListTile(
+                    title: Text("列表3"),
+                  ),
+                  ListTile(
+                    title: Text("列表3"),
+                  ),
+                  ListTile(
+                    title: Text("列表3"),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
     );
   }
 
